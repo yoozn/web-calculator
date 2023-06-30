96ec222fa881f5a4b5b2979d29ff5e92999cfd34
Brock Young <bearfoxfish@gmail.com>
2023-06-30 08:50:24 -0700

    Update cursor

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const buttons = document.querySelectorAll('button');
+const page = document.querySelector('*');
 
 
 function onHover(element) {
     element.addEventListener('mouseover', (e)=> {
         element.classList.add('hover-effect');
+        element.style.cursor = 'url("cursorHighlight.png"), default';
     });
     element.addEventListener('mouseleave',(e)=> {
         element.classList.remove('hover-effect');
+        element.style.cursor = 'url("cursor.png"), default';
     });
 }
 
